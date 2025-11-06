2322b3c91bcf9ca9a3b16f025be3225dd357e7d9
Fernando Ferreira <fernando.balieiro@outlook.com>
2025-11-06 12:29:53 -0300

    update folders for new architecture import cycle

--- back-end/backend/backend/serializers.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework import serializers
-from rest_framework.decorators import api_view
-from .models import Task
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['id', 'name', 'description', 'status', 'priority']
